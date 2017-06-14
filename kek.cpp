2d3e232b8342ea25458262efa1038f6fb95784af
Evan McNeil <evanmcneil1999@gmail.com>
2017-06-14 18:35:33 +0100

    Cleaned up code

--- kek.cpp
+++ kek.cpp
@@ -1,23 +1,31 @@
+#include <vector>
+#include <iostream>
+//#include <time.h>
+//#include <random>
+
 #include <cv.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/video/video.hpp>
-#include <vector>
-#include <iostream>
-#include <time.h>
-#include <random>
 
 #define PI 3.1416
 
 typedef std::pair<float, float> PolVec;
 typedef std::vector<PolVec> PolVecs;
 
+const bool debug = true;
+
 const float anglemargin = 0.3;
 const float sizemargin = 15;
-const int iters = 8;
+
 const int cropx = 200;
 const int cropy = 200;
+
+const float fovx = (60/360) * 2 * PI;
+const float fovy = fovx;
+
+const float depth = 5;
 
 PolVecs convertPolar(std::vector<cv::Point2f> &first, std::vector<cv::Point2f> &second)
 {
@@ -25,10 +33,12 @@ PolVecs convertPolar(std::vector<cv::Point2f> &first, std::vector<cv::Point2f> &
     for(int i = 0; i < first.size(); i++)
     {
         float diffx = second[i].x - first[i].x;
+        //Change +y to be up the image instead of down
         float diffy = first[i].y - second[i].y;
         float len = sqrt(pow(diffx, 2) + pow(diffy, 2));
         float angle = atan2(diffy, diffx);
-        //std::cout << diffx << "\t" << diffy << "\t" << angle << "  \t" << len << "\n";
+        //For debugging
+        if(debug) std::cout << diffx << "\t" << diffy << "\t" << angle << "  \t" << len << "\n";
         vals.push_back(PolVec(angle, len));
     }
     return vals;
@@ -76,33 +86,24 @@ int numAround(PolVecs &vecs, PolVec &center)
     return num;
 }
 
-/*
-PolVec bestFit(PolVecs vecs)
-{
-    PolVec closest(-1, -1);
-    for(int i = 0; i < iters; i++)
-    {
-        closest = avg(vecs, closest);
-    }
-    return closest;
-}
-*/
-
 PolVec bestFit(PolVecs vecs)
 {
     int best;
     int bestnum = 0;
-    srand(time(NULL));
+    //srand(time(NULL));
     for(int i = 0; i < vecs.size(); i++)
     {
+        //TODO: Decide whether to iterate through all points or choose random ones
         //int r = rand() % vecs.size();
         int rn = numAround(vecs, vecs[i]);
-        //std::cout << rn << "\n";
+        //For debugging
+        if(debug) std::cout << rn << "\n";
         if(rn > bestnum){bestnum = rn; best = i;}
     }
     return avg(vecs, vecs[best]);
 }
 
+//TODO: Put everything in a loop and take images through stdin
 int main(int argc, char* argv[])
 {
     cv::Mat img1, img2, img1c, img2c, img1b, img2b;
@@ -135,10 +136,12 @@ int main(int argc, char* argv[])
     }
 
     //Crop to get rid of warped area
+    //TODO: Run through image undistort instead
     img1c = img1(cv::Rect(centerx - cropx/2, centery - cropy/2,
                             centerx + cropx/2, centery + cropy/2));
     img2c = img2(cv::Rect(centerx - cropx/2, centery - cropy/2,
                             centerx + cropx/2, centery + cropy/2));
+
 
     //Convert to grayscale
     cv::cvtColor(img1c, img1b, cv::COLOR_BGR2GRAY);
@@ -152,6 +155,7 @@ int main(int argc, char* argv[])
     cv::calcOpticalFlowPyrLK(img1b, img2b, first, second, status, error);
 
     //Get rid of points that are not being tracked
+    //TODO: I don't think this is necessary, so taking out for now.
     /*
     for(int i = numfeatures - 1; i >= 0; i--)
     {
@@ -164,22 +168,35 @@ int main(int argc, char* argv[])
     }
     */
 
-    //TODO: Run vectors through image mapping
+    //TODO: Calculate change in distance between points to calculate change in depth.
+    //      This might be very hard.
 
     //Find best fit vector with angle and magnitude
     PolVec result = bestFit(convertPolar(first, second));
     
-    std::cout << result.first << "\t\t" << result.second << "\n";
+    if(debug) std::cout << result.first << "\t\t" << result.second << "\n";
 
-    for(int i = 0; i < numfeatures; i++)
+    //For now assume constant depth
+    //TODO: Get distance traveled in relative X and Y direction using actual depth.
+    float distx = sin(result.second * cos(result.first) / (float) cropx * fovx) * depth;
+    float disty = sin(result.second * sin(result.first) / (float) cropy * fovy) * depth;
+
+    std::cout << distx << "\t\t" << disty << "\n";
+
+    //For debugging
+    if(debug)
     {
-        if(status[i] == 1)
+        for(int i = 0; i < numfeatures; i++)
         {
-            cv::line(img2b, first[i], second[i], CV_RGB(255, 0, 0));
+            if(status[i] == 1)
+            {
+                cv::line(img2b, first[i], second[i], CV_RGB(255, 0, 0));
+            }
         }
+
+        cv::imshow("IMGFlow", img2b);
+        cv::waitKey(0);
     }
 
-    cv::imshow("IMGFlow", img2b);
-    cv::waitKey(0);
     return 0;
 }
